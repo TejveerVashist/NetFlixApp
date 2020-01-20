1188e2ed08970d8a9a96d93f7a4a22015595bb89
Tejveer <TEJVEER.LNU@aexp.com>
2020-01-20 17:06:16 +0530

    some changes

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,21 +1,6 @@
 import React from 'react';
 import style from './style.scss';
 import '../../global.css';
-
-/**
- * @render react
- * @name Navigation component
- * @description Main navigation for an app.
- * @example
- * <Navigation
- *    links={[
- *      { label: 'Browse', href: 'http://browse.to.somewhere' },
- *      { label: 'My List', href: '/my-list' },
- *      { label: 'Top Picks', href: '/top' },
- *      { label: 'Recent', href: '/recent' }
- *    ]}
- * />
- */
 const Navigation = ({ links }) => (
   <div id="navigation" className={style.Navigation}>
     <nav>
